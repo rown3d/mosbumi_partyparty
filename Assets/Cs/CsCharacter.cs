3f0f60be0b233792c84475cf9145e47808083364
rown3d <ritsu69@hotmail.com>
2025-01-24 23:07:26 +0100

    set up horizontal movement

--- Assets/Cs/CsCharacter.cs
+++ Assets/Cs/CsCharacter.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class CsCharacter : MonoBehaviour {
 
     Rigidbody m_pRigidbody;
+    float m_flInputX;
 
     // parameters
     [SerializeField] float m_flBounceSpeed;
+    [SerializeField] float m_flSpeedMultiplier;
 
     // start is called once before the first execution of update after the monobehaviour is created
     void Start() {
@@ -17,6 +19,19 @@ public class CsCharacter : MonoBehaviour {
     // update is called once per frame
     void Update() {
 
+        // get the inputs
+        m_flInputX = Input.GetAxisRaw("Horizontal");
+    }
+
+    // update fixed is called 50 times per second
+    void FixedUpdate() {
+
+        // horizontal movement
+        m_pRigidbody.linearVelocity = new Vector3(
+            m_flInputX * m_flSpeedMultiplier,
+            m_pRigidbody.linearVelocity.y,
+            m_pRigidbody.linearVelocity.z
+        );
     }
 
     // this function is called when the bounce event fires
@@ -26,6 +41,12 @@ public class CsCharacter : MonoBehaviour {
         Debug.Log("bounce");
 
         // bounce the character up
-        m_pRigidbody.linearVelocity = m_flBounceSpeed * transform.up;
+        //m_pRigidbody.linearVelocity = m_flBounceSpeed * transform.up;
+        //m_pRigidbody.linearVelocity.y = ;
+        m_pRigidbody.linearVelocity = new Vector3(
+            m_pRigidbody.linearVelocity.x,
+            m_flBounceSpeed,
+            m_pRigidbody.linearVelocity.z
+        );
     }
 }
