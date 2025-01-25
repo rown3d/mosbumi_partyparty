d7984217b94e3130ca163fc6dc48d9233e81e524
rown3d <ritsu69@hotmail.com>
2025-01-25 16:45:30 +0100

    set up game logic

---
+++ Assets/Cs/CsGame.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CsGame : MonoBehaviour {
+
+    [SerializeField] CsBubble m_pBubblePrefab;
+
+    // start is called once before the first execution of update after the monobehaviour is created
+    void Start() {
+
+        // put down a bunch of random bubbles
+        for (int nIndex = 0; nIndex <= 20; nIndex += 1) {
+
+            // instantiate a bubble
+            CsBubble pBubble = Instantiate(m_pBubblePrefab);
+
+            // give it a name
+            pBubble.name = "bubble";
+
+            // give it a random position
+            pBubble.transform.position = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(-4.0f, 4.0f), 0.0f);
+        }
+    }
+}
